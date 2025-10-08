72d89ab1fd1904e611a598d80f63f16897531e65
Islam <asoeslam@gmail.com>
2025-10-08 19:24:33 +0300

    Update app.js

--- nodeapp/app.js
+++ nodeapp/app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 app.get('/', (req, res) => {
-    res.send('Hello World from YAT DEPI! YAT 541')
+    res.send('Hello World from YAT DEPI! YAT 541 again by islam')
   })
   
   app.listen(port, () => {
